b0dbf542f6ef7957bdef5b28108da89162191333
Muzaffar <Muzaffareronboyev@gmail.com>
2023-05-01 08:02:00 -0400

    added js

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict"
+const numberOfSeries = +prompt("Nechta serial kordingiz","");
+
+let seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    actor:  {},
+    Genres: [],
+    privat:false;
+}
